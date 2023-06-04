a689a8b77735ff9018e3a590898981ced36f901b
이해승 <106800437+haeseung123@users.noreply.github.com>
2023-06-04 10:57:20 +0900

    2단계

---
+++ haeseung123/baekjoon/조건문/1330.js
@@ -0,0 +1,8 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split(' ').map(x => Number(x))
+
+let [a, b] = input
+
+if(a>b) console.log('>')
+else if (a<b) console.log('<')
+else if (a===b) console.log('==')
