98c98ec509743bfcfacead2f08be769c8dd7d022
이해승 <106800437+haeseung123@users.noreply.github.com>
2023-06-07 14:40:56 +0900

    3단계

---
+++ haeseung123/baekjoon/반복문/11021.js
@@ -0,0 +1,9 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n')
+
+for(let i=0; i<input.length; i++) {
+    input[i] = input[i].toString().split(' ').map(v => Number(v))
+    if(input[i].length > 1) {
+        console.log('Case #'+i+':', input[i][0]+input[i][1])
+    }
+}
