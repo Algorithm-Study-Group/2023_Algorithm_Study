10808547d800b2a3ba7961166614103d34aa2f6d
haeseung <showui96@gmail.com>
2023-06-25 17:22:42 +0900

    2941

--- haeseung123/baekjoon/심화 1/2947.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim()
-
-const alpha = input.replace(/c=|c-|dz=|d-|lj|nj|s=|z=/g , 0)
-console.log(alpha.split('').length)
